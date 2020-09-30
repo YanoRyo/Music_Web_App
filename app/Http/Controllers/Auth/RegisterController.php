25e0f7f02eeadf5f9d0ecfdfae677422c347104e
YanoRyo <yanoryo7032@gmail.com>
2020-09-30 15:04:45 +0900

    profile画像のバグの修正

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = 'contact/index';
+    protected $redirectTo = 'contact/postimage';
 
     /**
      * Create a new controller instance.
